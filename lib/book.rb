6632d53d18e8adfa4ce1c657338a9866ee5f6e62
Robert Hardy <rwhj77@hotmail.com>
2021-02-02 16:57:18 -0600

    commit tree ddb81051aed1d627caa68b5e894e307bc5160de4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
 
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+    
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"  
+    end
+
+
+
+end
+
+
+#book.new("Add Then There Were None:)
 
